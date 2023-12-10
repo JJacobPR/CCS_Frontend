b13da5fc1ae6ecbdf39a665124a6afd026d81caa
Jakub Janicki <jjanicki08@gmail.com>
2023-12-10 13:30:04 +0100

    tweak

--- src/components/user/Options.jsx
+++ src/components/user/Options.jsx
@@ -16,7 +16,7 @@ const Options = (props) => {
     }
 
     try {
-      const response = await fetch("http://localhost:3000/user", {
+      const response = await fetch("https://fileuploader-css.onrender.com/user", {
         method: "PUT",
         headers: { Authorization: `Bearer ${props.token}`, "Content-Type": "application/json" },
         body: { name, password },
