33ebc9ff84312a696df5a2276820ad6e115b20a5
Jakub Janicki <jjanicki08@gmail.com>
2023-12-12 18:38:25 +0100

    tweaks

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [new webpack.ProvidePlugin({ JSZip: "jszip" }), react()],
+  plugins: [react()],
 });
